acbc73c24edf34bf6c765ed9ccd273949ad00e07
Bora <Bora@gmail.com>
2025-07-20 09:12:41 +0700

    Add model related to checkout

--- E_Commerce/crm/accounts/models.py
+++ E_Commerce/crm/accounts/models.py
@@ -47,3 +47,49 @@ class TblSlide(models.Model):
 
     def __str__(self):
         return self.SlideName 
+class TblSubMenu(models.Model):
+    MenuID = models.ForeignKey(TblMenu, on_delete=models.CASCADE, null=True, blank=True)
+    SubMenuName = models.CharField(max_length=255, null=True)
+    SubMenuDescription = RichTextUploadingField(null=True, blank=True)
+
+    def __str__(self):
+        return self.SubMenuName
+    
+class Billing_details(models.Model):  
+    first_nume = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    companyname = models.CharField(max_length=255, null=True)
+    address = models.CharField(max_length=255, null=True)
+    Town = models.CharField(max_length=255, null=True)
+    country = models.CharField(max_length=255, null=True)
+    postcode = models.CharField(max_length=255, null=True)
+    Mobile = models.CharField(max_length=255, null=True)
+    Email = models.CharField(max_length=255, null=True)
+    Order_note = models.CharField(max_length=255, null=True)
+def __str__(self):
+        return self.first_name
+
+class Order(models.Model):
+     Customer = models.CharField(max_length=255, null=True)
+     TotalCost = models.DecimalField(max_digits=10, decimal_places=2)
+     Reciept = models.CharField(max_length=255, null=True)
+     order_date = models.DateField()
+def __str__(self):
+        return self.Customer
+
+class TblOrderDetail(models.Model):
+     OrderID = models.ForeignKey(Order, on_delete=models.CASCADE, null=True, blank=True)
+     Price = models.DecimalField(max_digits=10, decimal_places=2)
+     QUantity = models.DecimalField(max_digits=10, decimal_places=2)
+def __str__(self):
+        return self.OrderID
+class TblCustomer(models.Model):
+     CustomerName = models.CharField(max_length=255, null=True)
+     Phone = models.CharField(max_length=255, null=True)
+     Email = models.CharField(max_length=255, null=True)
+     Telegram = models.CharField(max_length=255, null=True)
+     Address = models.CharField(max_length=255, null=True)
+     Username = models.CharField(max_length=255, null=True)
+     Password = models.CharField(max_length=255, null=True)    
+def __str__(self):
+        return self.Username
